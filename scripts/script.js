a75f9f883db96f219b701636011b06917135f288
Kuba <Kuba@Patryks-Air.lan>
2023-12-26 16:15:31 +0100

    new communicates for bmi

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
-const wiek = document.getElementById("wiek");
-
-
+/*
 wiek.addEventListener(
     "focusout",
     function(item){
@@ -18,19 +16,53 @@ imie.addEventListener(
      }
     )
 
+*/
 
 const button = document.getElementById("submit");
 
-const wzrost = document.getElementById("wzrost");
 
-const waga = document.getElementById("waga");
-
-const bmi = Number(waga) / (Number(wzrost) * Number(wzrost));
 
 button.addEventListener(
     "click",
     function(item){
         item.preventDefault();
-        alert(bmi);
+        const wiek = document.getElementById("wiek").value;
+        const wzrost = document.getElementById("wzrost").value;
+        const mWzrost = Number(wzrost) / 100;
+        const waga = document.getElementById("waga").value;
+        const bmi = Number(waga) / (Number(mWzrost) * Number(mWzrost));
+
+        console.log('WZ:',wzrost, 'mWZ:',mWzrost,'Wag', waga,'BMI', bmi);
+
+        switch(bmi) {
+            case(Number(bmi) < 16):
+                console.log("You are only bonse")
+                break;
+            case(Number(bmi) >= 16)&&(Number(bmi) < 17):
+                console.log("You are slim")
+                break;
+            case(Number(bmi) >= 17)&&(Number(bmi) < 18.5):
+                console.log("You are skiny")
+                break;
+            case((Number(bmi) >= 18.5)&&(Number(bmi) < 24.9)):
+                console.log("You are ok");
+                break;
+            case((Number(bmi) >= 24.9)&&(Number(bmi) < 29.9)):
+                console.log("You are fluffy");
+                break;
+            case((Number(bmi) >= 29.8)&&(Number(bmi) < 34.9)):
+                console.log("You are big")
+                break;
+            case((Number(bmi) >= 34.9)&&(Number(bmi) < 39.9)):
+                console.log("You are MONSTER")
+                break;
+            case(Number(bmi) >= 39.9):
+                console.log("You are GORLOCK THE DESTROYER OF WORLDS!!!")
+                break;
+        
+
+        }
+        
+        
     }
 )
