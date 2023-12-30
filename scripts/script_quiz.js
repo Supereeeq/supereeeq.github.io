e43ea7129aa3453936e5600f9053ac8b00f7233e
Supereeeq <152003399+Supereeeq@users.noreply.github.com>
2023-12-30 18:53:24 +0100

    making quiz1

--- scripts/script_quiz.js
+++ scripts/script_quiz.js
@@ -3,6 +3,7 @@ showQuestion(questions);
 
 function showQuestion(questions) {
     const quiz = document.getElementById("quiz");
+
     questions.forEach((item, index) => {
         
         if (index === 0){
@@ -12,12 +13,53 @@ function showQuestion(questions) {
             pytanie.innerText = `${item}`;
             quiz.appendChild(pytanie);
         }
-        else{
-            const odpowiedz = document.createElement("div");
-            odpowiedz.id = ("odpowiedz");
-            odpowiedz.classList.add ("odpowiedz");
-            odpowiedz.innerText = `${item}`;
-            quiz.appendChild(odpowiedz);
+        else if (index === 1){
+            const odpowiedzInput = document.createElement("input");
+            odpowiedzInput.type = "radio";
+            odpowiedzInput.classList.add ("odpowiedz1");
+            odpowiedzInput.classList.add = ("odpowiedz");
+            quiz.appendChild(odpowiedzInput);
+            const odpowiedzLabel = document.createElement("label");
+            odpowiedzLabel.classList.add ("odpowiedz-label1");
+            odpowiedzLabel.classList.add ("odpowiedz-label");
+            odpowiedzLabel.innerText = `${item}`;
+            quiz.appendChild(odpowiedzLabel);
+        }else if (index === 2){
+            const odpowiedzInput = document.createElement("input");
+            odpowiedzInput.type = "radio";
+            odpowiedzInput.classList.add = ("odpowiedz2");
+            odpowiedzInput.classList.add = ("odpowiedz");
+            quiz.appendChild(odpowiedzInput);
+            const odpowiedzLabel = document.createElement("label");
+            odpowiedzLabel.id = ("odpowiedz");
+            odpowiedzLabel.classList.add ("odpowiedz-label2");
+            odpowiedzLabel.classList.add ("odpowiedz-label");
+            odpowiedzLabel.innerText = `${item}`;
+            quiz.appendChild(odpowiedzLabel);
+        }else if (index === 3){
+            const odpowiedzInput = document.createElement("input");
+            odpowiedzInput.type = "radio";
+            odpowiedzInput.classList.add = ("odpowiedz3");
+            odpowiedzInput.classList.add = ("odpowiedz");
+            quiz.appendChild(odpowiedzInput);
+            const odpowiedzLabel = document.createElement("label");
+            odpowiedzLabel.id = ("odpowiedz");
+            odpowiedzLabel.classList.add ("odpowiedz-label3");
+            odpowiedzLabel.classList.add ("odpowiedz-label");
+            odpowiedzLabel.innerText = `${item}`;
+            quiz.appendChild(odpowiedzLabel);
+        }else if (index === 4){
+            const odpowiedzInput = document.createElement("input");
+            odpowiedzInput.type = "radio";
+            odpowiedzInput.classList.add = ("odpowiedz4");
+            odpowiedzInput.classList.add = ("odpowiedz");
+            quiz.appendChild(odpowiedzInput);
+            const odpowiedzLabel = document.createElement("label");
+            odpowiedzLabel.id = ("odpowiedz");
+            odpowiedzLabel.classList.add ("odpowiedz-label4");
+            odpowiedzLabel.classList.add ("odpowiedz-label");
+            odpowiedzLabel.innerText = `${item}`;
+            quiz.appendChild(odpowiedzLabel);
         }
     });
 }
