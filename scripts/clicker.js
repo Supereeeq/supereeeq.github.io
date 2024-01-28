466fb92e9917c7ed2164a80678c2cfa1f19471d9
Supereeeq <152003399+Supereeeq@users.noreply.github.com>
2024-01-28 23:51:20 +0100

    many things
    upgraded clicker, started working on quiz

--- scripts/clicker.js
+++ scripts/clicker.js
@@ -1,7 +1,8 @@
-const clicked = document.getElementById("zdj-carrot");
+const clicked = document.getElementById("carrot");
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
+let plus = document.getElementById("plusJeden")
 let points = 0;
 let p = 0;
 let q = 0;
@@ -35,6 +36,27 @@ function showButton1() {
       break;
     case 3:
       opis1.textContent = "150";
+      break;
+    case 4:
+      opis1.textContent = "200";
+      break;
+    case 5:
+      opis1.textContent = "250";
+      break;
+    case 6:
+      opis1.textContent = "300";
+      break;
+    case 7:
+      opis1.textContent = "400";
+      break;
+    case 8:
+      opis1.textContent = "500";
+      break;
+    case 9:
+      opis1.textContent = "750";
+      break;
+    deafult:
+      opis1.textContent = "Kupiłeś wszystko"
       break;
   }
 
@@ -70,7 +92,28 @@ function showButton2() {
       opis2.textContent = "500";
       break;
     case 2:
-      opis2.textContent = "100";
+      opis2.textContent = "1000";
+      break;
+    case 3:
+      opis2.textContent = "1500";
+      break;
+    case 4:
+      opis2.textContent = "2000";
+      break;
+    case 5:
+      opis2.textContent = "2500";
+      break;
+    case 6:
+      opis2.textContent = "5000";
+      break;
+    case 7:
+      opis2.textContent = "7500";
+      break;
+    case 8:
+      opis2.textContent = "10000";
+      break;
+    case 9:
+      opis2.textContent = "15000";
       break;
   }
 
@@ -92,14 +135,26 @@ function showButton2() {
 function backgroundPoints(){
   points++;
   showPoints(points);
+  let q = 0;
+  animatePlus(q);
 }
-
 
 clicked.addEventListener("click", function() {
   clicked.classList.add("zdj-carrot-clicked");
-  howManyAdd(q)
+  onClick(q)
 
 });
+
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Space') {
+    onClick(q)
+  }
+});
+
+function onClick() {
+  howManyAdd(q)
+  animatePlus(q)
+}
 
 function howManyAdd(q){
   switch(q){
@@ -172,9 +227,67 @@ function changeButton1(){
         points = points - 150;
         p++;
         showPoints();
-        showButtons(p);
+        showButton1(p);
         setInterval(backgroundPoints, 3000);
       }
+      case 4:
+        if (points >= 200)
+        {
+          points = points - 200;
+          p++;
+          showPoints(points);
+          showButton1(p);
+          setInterval(backgroundPoints, 2500);
+        }
+        break;
+      case 5:
+        if (points >= 250)
+        {
+          points = points - 250;
+          p++;
+          showPoints();
+          showButton1(p);
+          setInterval(backgroundPoints, 2000);
+        }
+      break;
+      case 6:
+        if (points >= 300)
+        {
+          points = points - 300;
+          p++;
+          showPoints();
+          showButton1(p);
+          setInterval(backgroundPoints, 1800);
+        }
+        case 7:
+        if (points >= 400)
+        {
+          points = points - 400;
+          p++;
+          showPoints();
+          showButton1(p);
+          setInterval(backgroundPoints, 1500);
+        }    
+      case 8:
+      if (points >= 500)
+      {
+        points = points - 500;
+        p++;
+        showPoints(points);
+        showButton1(p);
+        setInterval(backgroundPoints, 1000);
+      }
+      break;
+    case 9:
+      if (points >= 750)
+      {
+        points = points - 750;
+        p++;
+        showPoints();
+        showButton1(p);
+        setInterval(backgroundPoints, 900);
+      }
+    break;
   }
   
 }
@@ -195,7 +308,7 @@ function changeButton2(){
       {
         points = points - 200;
         q++;
-        showPoints(points);
+        showPoints();
         showButton2(q);
         howManyAdd();
       }
@@ -209,7 +322,7 @@ function changeButton2(){
         showButton2(q);
         howManyAdd();
       }
-    break;
+      break;
     case 2:
       if (points >= 1000)
       {
@@ -219,6 +332,121 @@ function changeButton2(){
         showButton2(q);
         howManyAdd();
       }
+      break;
+    case 3:
+      if (points >= 1500)
+      {
+        points = points - 1500;
+        q++;
+        showPoints(points);
+        showButton2(q);
+        howManyAdd();
+      }
+      break;
+    case 4:
+      if (points >= 2000)
+      {
+        points = points - 2000;
+        q++;
+        showPoints();
+        showButton2(q);
+        howManyAdd();
+      }
+      break;
+    case 5:
+      if (points >= 2500)
+      {
+        points = points - 2500;
+        q++;
+        showPoints(points);
+        showButton2(q);
+        howManyAdd();
+      }
+      break;
+    case 6:
+      if (points >= 5000)
+      {
+        points = points - 5000;
+        q++;
+        showPoints();
+        showButton2(q);
+        howManyAdd();
+      }
+      break;
+    case 7:
+      if (points >= 7500)
+      {
+        points = points - 7500;
+        q++;
+        showPoints();
+        showButton2(q);
+        howManyAdd();
+      }
+      break;
+    case 8:
+      if (points >= 10000)
+      {
+        points = points - 10000;
+        q++;
+        showPoints();
+        showButton2(q);
+        howManyAdd();
+      }
+      break;
+    case 9:
+      if (points >= 15000)
+      {
+        points = points - 15000;
+        q++;
+        showPoints();
+        showButton2(q);
+        howManyAdd();
+      }
+      break;
   }
-  
+}
+
+function animatePlus (q)
+{
+  let plus = document.createElement("img");
+  plus.classList.add("plus");
+  switch(q){
+    case 0:
+      plus.src = "../images/plus1.png";
+      break;
+    case 1:
+      plus.src = "../images/plus2.png";
+      break;
+    case 2:
+      plus.src = "../images/plus4.png";
+      break;
+    case 3:
+      plus.src = "../images/plus8.png";
+      break;
+  }
+
+  let positionX = Math.floor(Math.random() * (901 - 400) + 400);
+  let positionY = Math.floor(Math.random() * (501 - 400) + 400);
+  plus.style.left = positionX + 'px';
+  plus.style.top = positionY + 'px';
+  document.body.appendChild(plus);
+
+  plus.addEventListener('click', function() {
+    onClick(q);
+});
+
+  let intervalId = setInterval(function() {
+      moveUp(plus, intervalId);
+  }, 20);
+}
+
+function moveUp(plus, intervalId) {
+  let currentTop = parseInt(plus.style.top) || positionY;
+  if (currentTop > 180) {
+      plus.style.top = (currentTop - 2) + 'px';
+  } 
+  else {
+    plus.style.display="none"
+    clearInterval(intervalId);
+  }
 }
--- scripts/script_quiz.js
+++ scripts/script_quiz.js
@@ -1,3 +1,200 @@
+let zapytanie = {
+    pytanie: "Co?",
+    odpowiedz1: "A",
+    odpowiedz2: "B",
+    odpowiedz3: "C",
+    odpowiedz4: "D",
+    poprawnaOdpowiedz: "B"
+}
+
+let quiz = document.getElementById("quiz")
+let submit = document.getElementById("submit")
+let numberOfLifes = 0;
+let numberOfWins = 0;
+showNumberOfLifes(numberOfLifes);
+zapytaj();
+
+
+function zapytaj() {
+    if(numberOfLifes !== 0){
+
+        let pytanie = document.createElement("div");
+        pytanie.innerText = zapytanie.pytanie;
+        pytanie.classList.add("pytanie")
+        quiz.appendChild(pytanie);
+
+        let odpowiedz1 = document.createElement("div");
+        odpowiedz1.classList.add("odpowiedz");
+        quiz.appendChild(odpowiedz1);
+
+            let zawartosc1 = document.createElement("input");
+            zawartosc1.setAttribute("type", "radio");
+            zawartosc1.setAttribute("name", "odpowiedz")
+            zawartosc1.id = "odpowiedz1"
+            odpowiedz1.appendChild(zawartosc1)
+
+            let label1 = document.createElement("label");
+            label1.setAttribute("for", "odpowiedz1");
+            label1.textContent = zapytanie.odpowiedz1;
+            odpowiedz1.appendChild(label1)
+
+        let odpowiedz2 = document.createElement("div");
+        odpowiedz2.classList.add("odpowiedz");
+        quiz.appendChild(odpowiedz2);
+
+            let zawartosc2 = document.createElement("input");
+            zawartosc2.setAttribute("type", "radio");
+            zawartosc2.setAttribute("name", "odpowiedz")
+            zawartosc2.id = "odpowiedz2"
+            odpowiedz2.appendChild(zawartosc2)
+
+            let label2 = document.createElement("label");
+            label2.setAttribute("for", "odpowiedz2");
+            label2.textContent = zapytanie.odpowiedz2;
+            odpowiedz2.appendChild(label2)
+
+        let odpowiedz3 = document.createElement("div");
+        odpowiedz3.classList.add("odpowiedz");
+        quiz.appendChild(odpowiedz3);
+
+            let zawartosc3 = document.createElement("input");
+            zawartosc3.setAttribute("type", "radio");
+            zawartosc3.setAttribute("name", "odpowiedz")
+            zawartosc3.id = "odpowiedz3"
+            odpowiedz3.appendChild(zawartosc3)
+
+            let label3 = document.createElement("label");
+            label3.setAttribute("for", "odpowiedz3");
+            label3.textContent = zapytanie.odpowiedz3;
+            odpowiedz3.appendChild(label3)
+
+        let odpowiedz4 = document.createElement("div");
+        odpowiedz4.classList.add("odpowiedz");
+        quiz.appendChild(odpowiedz4);
+        
+            let zawartosc4 = document.createElement("input");
+            zawartosc4.setAttribute("type", "radio");
+            zawartosc4.setAttribute("name", "odpowiedz")
+            zawartosc4.id = "odpowiedz4"
+            odpowiedz4.appendChild(zawartosc4)
+
+            let label4 = document.createElement("label");
+            label4.setAttribute("for", "odpowiedz4");
+            label4.textContent = zapytanie.odpowiedz4;
+            odpowiedz4.appendChild(label4)
+
+    }
+    else
+    {
+        let healthBar = document.getElementById("healthBar")
+
+        submit.classList.add("noLives")
+        healthBar.classList.add("noLives")
+
+    }
+}
+
+function showNumberOfLifes () 
+{
+    if(numberOfLifes !== 0){
+        let healthBar = document.getElementById("healthBar")
+
+        healthBar.innerHTML = ""; //Usuwanie zawartości 
+
+        let napisIlosciZyc = document.createElement("div");
+        napisIlosciZyc.classList.add("napisIloscZyc")
+        napisIlosciZyc.innerText = "Ilość żyć: " + numberOfLifes;
+        healthBar.appendChild(napisIlosciZyc);
+
+        if(numberOfLifes > 2)
+        {
+            let life3 = document.createElement("div");
+            life3.classList.add("life3");
+            life3.classList.add("life");
+            healthBar.appendChild(life3);
+        }
+        if(numberOfLifes > 1)
+        {
+            let life2 = document.createElement("div");
+            life2.classList.add("life2");
+            life2.classList.add("life");
+            healthBar.appendChild(life2);
+        }
+        if(numberOfLifes > 0)
+        {
+            let life1 = document.createElement("div");
+            life1.classList.add("life1");
+            life1.classList.add("life");
+            healthBar.appendChild(life1);
+        }
+        if(numberOfLifes < 1)
+        {
+            console.log("Koniec żyć")
+        }  
+    }
+    else
+    {
+        let koniec = document.createElement("div");
+            koniec.classList.add("koniec");
+            koniec.innerText = "Koniec gry"
+
+            let iloscWygranych = document.createElement("div")
+            iloscWygranych.classList.add("iloscWygranych");
+            iloscWygranych.innerText = "Ilość wygranych to: " + numberOfWins;
+
+            let playAgain = document.createElement("div")
+            playAgain.classList.add("playAgain")
+
+            quiz.appendChild(koniec);
+            koniec.appendChild(iloscWygranych);
+            koniec.appendChild(playAgain);
+        
+    }
+}
+
+
+submit.addEventListener("click", function() {
+    if(numberOfLifes !== 0){
+        const radioInputs = document.querySelectorAll('input[type="radio"][name="odpowiedz"]');
+    
+        let checkedLabelValue = null;
+
+        radioInputs.forEach(function(input) {
+            if (input.checked) {
+                const label = input.parentElement.querySelector("label");
+                checkedLabelValue = label.textContent;
+                return;
+            }
+        });
+
+        if (checkedLabelValue !== null) {
+            if (checkedLabelValue === zapytanie.poprawnaOdpowiedz) 
+            {
+                console.log("Correct answer!");
+                numberOfWins ++;
+
+            } 
+            else 
+            {
+                console.log("Incorrect answer!");
+
+                numberOfLifes -= 1;
+                showNumberOfLifes(numberOfLifes);
+            }
+        } 
+        else 
+        {
+            console.log("No input is checked.");
+
+        }
+    }
+});
+
+
+
+
+
+/*
 // const questions = ["Co wspiera zdrowe kości?", "A) Żelazo", "B) Magnez", "C) Wapń", "D) Potas"];
 // showQuestion(questions);
 
@@ -225,4 +422,4 @@ questionsArray.map((obj) => {
 //     }
 // )
 
-
+*/
