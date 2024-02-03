f0e3f7ff7c725971d7dced7e4df05a3f53c2e089
Supereeeq <152003399+Supereeeq@users.noreply.github.com>
2024-02-04 00:34:31 +0100

    store v1.2
    filtering in store now works

--- scripts/sklep.js
+++ scripts/sklep.js
@@ -1,24 +1,130 @@
-let waga = document.querySelectorAll('input[name="waga"]')
+let produkty = 
+[
+    {
+        name:"ryż niskokaloryczny",
+        cena:19.99,
+        image:"../images/ryz.png",
+        link:"./sklep/ryz.html",
+        tags:"ryż niskokaloryczny dieta chudnięcie",
+        type:"dieta",
+        stars:4
+    },
+    {
+        name:"ryż niskokaloryczny",
+        cena:29.99,
+        image:"../images/ryz.png",
+        link:"./sklep/ryz.html",
+        tags:"ryż niskokaloryczny dieta chudnięcie",
+        type:"dieta",
+        stars:4
+    },
+    {
+        name:"ryż niskokaloryczny",
+        cena:39.99,
+        image:"../images/ryz.png",
+        link:"./sklep/ryz.html",
+        tags:"ryż niskokaloryczny dieta chudnięcie",
+        type:"ubranie",
+        stars:4
+    },
+    {
+        name:"ryż niskokaloryczny",
+        cena:19.99,
+        image:"../images/ryz.png",
+        link:"./sklep/ryz.html",
+        tags:"ryż niskokaloryczny dieta chudnięcie",
+        type:"plan",
+        stars:4
+    },
+    {
+        name:"ryż niskokaloryczny",
+        cena:49.99,
+        image:"../images/ryz.png",
+        link:"./sklep/ryz.html",
+        tags:"ryż niskokaloryczny dieta chudnięcie",
+        type:"jedzenie",
+        stars:4
+    },
+]
+
+//kategorie
+let ocena = document.getElementById("ranking")
+let napisOceny = document.getElementById("pokazOceny")
 
 let cena = document.getElementById("cena")
-let napis = document.getElementById("pokazCeny")
+let napisCeny = document.getElementById("pokazCeny")
+
+let jedzenie = document.getElementById("jedzenie")
+let ubranie = document.getElementById("ubranie")
+let dieta = document.getElementById("dieta")
+let plan = document.getElementById("plan")
+
+
+let przedmioty = document.getElementById("przedmioty")
+
+let szukaj = document.getElementById("find")
+
+generuj()
 
 
 cena.addEventListener("input",function(){
-    napis.innerHTML = ""
+    napisCeny.innerHTML = ""
 
-    console.log(cena.value)
     let obecnaCena = document.createElement("div")
-    obecnaCena.innerText = "Cena: " + cena.value + "zł"
+    obecnaCena.innerText = "Max. Cena: " + cena.value + "zł"
     obecnaCena.classList.add("obecnaCena")
 
-    napis.appendChild(obecnaCena)
+    napisCeny.appendChild(obecnaCena)
 })
 
+ocena.addEventListener("input",function(){
+    napisOceny.innerHTML = ""
+    let obecnaOcena = document.createElement("div")
+    obecnaOcena.innerText = "Min. Ocena: " + ranking.value
 
-/*
-waga.forEach(function(waga) {
-    if (waga.checked) {
+    napisOceny.appendChild(obecnaOcena)
+})
+
+szukaj.addEventListener("click",function()
+{
+    generuj();
+})
+
+function generuj() {
+    przedmioty.innerHTML = "";
+    let numerOkna = 1;
+
+    for (let i = 0; i < produkty.length; i++) 
+    {
+
+        if (cena.value >= produkty[i].cena) 
+        {
+
+            if (jedzenie.checked || ubranie.checked || dieta.checked || plan.checked) 
+            {
+                console.log("Product type: " + produkty[i].type);
+
+                if (
+                    (jedzenie.checked && produkty[i].type === "jedzenie") ||
+                    (ubranie.checked && produkty[i].type === "ubranie") ||
+                    (dieta.checked && produkty[i].type === "dieta") ||
+                    (plan.checked && produkty[i].type === "plan")
+                )
+                {
+                    console.log("KYS")
+
+                   
+                        console.log("JS")
+                        
+                        let okno = document.createElement("div");
+                        okno.classList.add("okno");
+                        okno.classList.add("okno" + numerOkna);
+                        przedmioty.appendChild(okno);
+                        numerOkna++;
+                    
+                    
+                }
+            }
+        }
     }
-})
-*/
+}
