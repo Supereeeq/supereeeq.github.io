01460d09cf5717fcb90f3bb724a0b8186d064961
Supereeeq <152003399+Supereeeq@users.noreply.github.com>
2024-02-04 23:38:08 +0100

    shop v1.3
    Added rows, and images and products

--- scripts/sklep.js
+++ scripts/sklep.js
@@ -1,50 +1,113 @@
 let produkty = 
 [
     {
-        name:"ryż niskokaloryczny",
+        name:"Ryż niskokaloryczny",
         cena:19.99,
-        image:"../images/ryz.png",
+        image:"../images/ryz.jpg",
         link:"./sklep/ryz.html",
         tags:"ryż niskokaloryczny dieta chudnięcie",
         type:"dieta",
-        stars:4
+        stars:2
     },
     {
-        name:"ryż niskokaloryczny",
-        cena:29.99,
-        image:"../images/ryz.png",
-        link:"./sklep/ryz.html",
-        tags:"ryż niskokaloryczny dieta chudnięcie",
-        type:"dieta",
-        stars:4
+        name: "Mieszanka orzechowa",
+        cena: 29.99,
+        image: "../images/mieszanka_orzechowa.jpg",
+        link: "./sklep/mieszanka_orzechowa.html",
+        tags: "owoce orzechy zdrowe przekąski",
+        type: "jedzenie",
+        stars: 4
+    },    
+    {
+        name: "Chia Seeds Organic",
+        cena: 15.49,
+        image: "../images/chia_seeds.jpg",
+        link: "./sklep/chia_seeds.html",
+        tags: "chia nasiona organiczne superfoods",
+        type: "jedzenie",
+        stars: 3
     },
     {
-        name:"ryż niskokaloryczny",
-        cena:39.99,
-        image:"../images/ryz.png",
-        link:"./sklep/ryz.html",
-        tags:"ryż niskokaloryczny dieta chudnięcie",
-        type:"ubranie",
-        stars:4
+        name: "Kwasy omega-3",
+        cena: 24.99,
+        image: "../images/omega3.jpg",
+        link: "./sklep/omega3.html",
+        tags: "omega-3 zdrowa dieta suplementy",
+        type: "dieta",
+        stars: 4
     },
     {
-        name:"ryż niskokaloryczny",
-        cena:19.99,
-        image:"../images/ryz.png",
-        link:"./sklep/ryz.html",
-        tags:"ryż niskokaloryczny dieta chudnięcie",
-        type:"plan",
-        stars:4
+        name: "Legginsy sportowe",
+        cena: 49.99,
+        image: "../images/legginsy_sportowe.jpg",
+        link: "./sklep/legginsy_sportowe.html",
+        tags: "ubrania sportowe fitness legginsy",
+        type: "ubranie",
+        stars: 2
     },
     {
-        name:"ryż niskokaloryczny",
-        cena:49.99,
-        image:"../images/ryz.png",
-        link:"./sklep/ryz.html",
-        tags:"ryż niskokaloryczny dieta chudnięcie",
-        type:"jedzenie",
-        stars:4
+        name: "Koszulka termoaktywna",
+        cena: 34.99,
+        image: "../images/koszula_termoaktywna.jpg",
+        link: "./sklep/koszula_termoaktywna.html",
+        tags: "odzież sportowa termoaktywna fitness",
+        type: "ubranie",
+        stars: 4
     },
+    {
+        name: "Trening ABS",
+        cena: 59.99,
+        image: "../images/trening_brzucha.jpg",
+        link: "./sklep/trening_brzucha.html",
+        tags: "trening brzuch fitness plan",
+        type: "plan",
+        stars: 1
+    },
+    {
+        name: "Kettlebell Workout Plan",
+        cena: 69.99,
+        image: "../images/kettlebell.jpg",
+        link: "./sklep/kettlebell.html",
+        tags: "trening kettlebell siła fitness",
+        type: "plan",
+        stars: 5
+    },    
+    {
+        name: "Quinoa Organiczna",
+        cena: 12.99,
+        image: "../images/quinoa.jpg",
+        link: "./sklep/quinoa.html",
+        tags: "quinoa zdrowe jedzenie organiczne",
+        type: "jedzenie",
+        stars: 4
+    },
+    {
+        name: "Mieszanka superfoods",
+        cena: 49.99,
+        image: "../images/mieszanka_superfoods.jpg",
+        link: "./sklep/superfoods.html",
+        tags: "superfoods dieta zdrowie",
+        type: "dieta",
+        stars: 3
+    },
+    {
+        name: "Buty do biegania",
+        cena: 89.99,
+        image: "../images/buty_do_biegania.jpg",
+        link: "./sklep/buty_bieganie.html",
+        tags: "buty sportowe bieganie fitness",
+        type: "ubranie",
+        stars: 4
+    },
+    {
+        name: "Yoga Guide",
+        cena: 29.99,
+        image: "../images/yoga.jpg",
+        link: "./sklep/yoga.html",
+        tags: "yoga plan treningowy mindfulness",
+        type: "plan",
+        stars: 4
+    },                
 ]
 
 //kategorie
@@ -94,16 +157,35 @@ function generuj() {
     przedmioty.innerHTML = "";
     let numerOkna = 1;
 
+    let rzadPierwszy = document.createElement("div")
+    rzadPierwszy.classList.add("rzad")
+    rzadPierwszy.classList.add("rzad1")
+
+
+    let rzadDrugi = document.createElement("div")
+    rzadDrugi.classList.add("rzad")
+    rzadDrugi.classList.add("rzad2")
+
+
+    let rzadTrzeci = document.createElement("div")
+    rzadTrzeci.classList.add("rzad")
+    rzadTrzeci.classList.add("rzad3")
+
+
+    let rzadCzwarty = document.createElement("div")
+    rzadCzwarty.classList.add("rzad")
+    rzadCzwarty.classList.add("rzad4")
+
+    let rzadPiaty = document.createElement("div")
+    rzadPiaty.classList.add("rzad")
+    rzadPiaty.classList.add("rzad5")
+
     for (let i = 0; i < produkty.length; i++) 
     {
-
         if (cena.value >= produkty[i].cena) 
         {
-
             if (jedzenie.checked || ubranie.checked || dieta.checked || plan.checked) 
             {
-                console.log("Product type: " + produkty[i].type);
-
                 if (
                     (jedzenie.checked && produkty[i].type === "jedzenie") ||
                     (ubranie.checked && produkty[i].type === "ubranie") ||
@@ -111,18 +193,84 @@ function generuj() {
                     (plan.checked && produkty[i].type === "plan")
                 )
                 {
-                    console.log("KYS")
+                    if 
+                    (
+                        produkty[i].cena <= cena.value &&
+                        produkty[i].stars >= ranking.value
+                    )
+                    {
 
-                   
-                        console.log("JS")
-                        
                         let okno = document.createElement("div");
                         okno.classList.add("okno");
                         okno.classList.add("okno" + numerOkna);
-                        przedmioty.appendChild(okno);
+                        okno.innerHTML = '<div class="zdj' + numerOkna +' zdj"><img src="' + produkty[i].image +'"></div><div class="opis' + numerOkna + ' opis"><p>' + produkty[i].name + '</p></div>'
+                
+                        let ocenaProduktu = document.createElement("div");
+                        ocenaProduktu.classList.add("ocenaProduktu");
+
+                        
+                        let iloscOceny = produkty[i].stars;
+                        
+                        while (iloscOceny > 0) 
+                        {
+                            if (iloscOceny >= 1) 
+                            {         
+                                let gwiazdka = document.createElement("img");
+                                gwiazdka.src = '../images/star.png';
+
+                                iloscOceny -= 1;
+                                ocenaProduktu.appendChild(gwiazdka);
+                                console.log("wykonane -1")
+
+                            } else if (iloscOceny >= 0.5) 
+                            {                     
+                                let polGwiazdki = document.createElement("img");
+                                polGwiazdki.src = '../images/halfStar.png';
+
+                                iloscOceny -= 0.5;
+                                ocenaProduktu.appendChild(polGwiazdki);
+                               
+                            }
+                        }
+                        
+                        okno.appendChild(ocenaProduktu);
+
+
+                        let cenaProduktu = document.createElement("div")
+                        cenaProduktu.classList.add("cenaProduktu")
+                        cenaProduktu.innerText = produkty[i].cena + "zł"
+
+                        okno.appendChild(cenaProduktu)
+
+                        if(numerOkna < 4)
+                        {
+                            rzadPierwszy.appendChild(okno)
+                            przedmioty.append(rzadPierwszy)
+                        }
+                        else if(numerOkna < 7)
+                        {    
+                            przedmioty.append(rzadDrugi)
+                            rzadDrugi.appendChild(okno)
+                        }
+                        else if(numerOkna < 10)
+                        {    
+                            przedmioty.append(rzadTrzeci)
+                            rzadTrzeci.appendChild(okno)
+                        }
+                        else if(numerOkna < 13)
+                        {    
+                            przedmioty.append(rzadCzwarty)
+                            rzadCzwarty.appendChild(okno)
+                        }
+                        else if(numerOkna < 16)
+                        {
+                            przedmioty.appendChild(rzadPiaty)
+                            rzadPiaty.appendChild(okno)
+                        }
+
                         numerOkna++;
                     
-                    
+                    }
                 }
             }
         }
